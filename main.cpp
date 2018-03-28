56e8b0bfdfd9e34129a79917a8dcd1d4c327d191
Vitor Peixoto <vitor_peixoto@outlook.com>
2018-03-28 14:53:38 -0300

    Telas de pause/sair/reiniciar e inicio da HUD

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GL/freeglut.h>
 #include <iostream>
 #include <math.h>
+#include <string.h>
 
 #include "include/Spaceship.h"
 #include "include/Map.h"
@@ -16,16 +17,19 @@ using namespace std;
 #define Y 1
 #define Z 2
 
+#define KEY_ESC 27
 #define MULTIPLIER 0.5
 #define FPS_CONST 0.017*MULTIPLIER
+#define HUD_MARGIN 20
 
 bool* keyStates = new bool[127];
 bool* specialKeyStates = new bool[127];
+bool isPaused = false, isExiting = false, isRestarting = false;
 
 int windowWidth,
     windowHeight;
 
-double oWidth, oHeight;
+double orthoHalfWidth, orthoHalfHeight;
 
 Spaceship s(0.0, 0.0, 26.0, 40.0, 20.0);
 Map m;
@@ -34,15 +38,66 @@ Vector3d movement(0.0, 0.0, 0.0);
 double gravity = -9.8;
 Vector3d gravityVector(0.0, gravity*FPS_CONST, 0.0);
 
+void drawText(char* s, float x, float y) {
+    unsigned int i;
+    glRasterPos2f(x, y);
 
+    for (i = 0; i < strlen (s); i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, s[i]);
+    }
+}
 
+double getStringBitmapLength(char* str) {
+    return ((double)glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned const char*) str)/windowWidth)*(orthoHalfWidth*2);
+}
+
+void drawRestartingConfirmation() {
+    glColor3f(1.0, 1.0, 1.0);
+    char *message = (char*)"Voce tem certeza que deseja reiniciar o jogo?";
+    drawText(message, -getStringBitmapLength(message)/2, 0);
+
+    message = (char*)"S/N";
+    drawText("S/N", -getStringBitmapLength(message)/2, -20);
+}
+
+void drawExitingConfirmation() {
+    glColor3f(1.0, 1.0, 1.0);
+    char *message = (char*)"Voce tem certeza que deseja sair do jogo?";
+    drawText(message, -getStringBitmapLength(message)/2, 0);
+
+    message = (char*)"S/N";
+    drawText("S/N", -getStringBitmapLength(message)/2, -20);
+}
+
+void drawPaused() {
+    glColor3f(1.0, 1.0, 1.0);
+    char *message = (char*)"Pausado";
+    drawText(message, -getStringBitmapLength(message)/2, 0);
+}
+
+void drawHUD() {
+    glColor3f(1.0, 1.0, 1.0);
+
+    char *message = (char*)"Altura:";
+    drawText(message, -orthoHalfWidth+HUD_MARGIN, orthoHalfHeight-20);
+
+    message = (char*)"Velocidade:";
+    drawText(message, -orthoHalfWidth+HUD_MARGIN, orthoHalfHeight-40);
+
+    message = (char*)"Aceleracao:";
+    drawText(message, -orthoHalfWidth+HUD_MARGIN, orthoHalfHeight-60);
+}
 
 void drawScene(void) {
     // Limpa a tela (com a cor definida por glClearColor(r,g,b)) para que
     // possamos desenhar
     glClear(GL_COLOR_BUFFER_BIT);
-        s.drawSpaceship();
         m.drawMap();
+        s.drawSpaceship();
+        if(isRestarting) drawRestartingConfirmation();
+        if(isExiting)    drawExitingConfirmation();
+        if(isPaused)     drawPaused();
+        drawHUD();
     // Diz ao OpenGL para colocar o que desenhamos na tela
     glutSwapBuffers();
 }
@@ -96,11 +151,11 @@ void resizeScreen(int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    oWidth  = 500 * aspectRatio;
-    oHeight = 500;
+    orthoHalfWidth  = 500 * aspectRatio;
+    orthoHalfHeight = 500;
 
-    glOrtho(-oWidth, oWidth, -oHeight, oHeight, -1, 1);
-    m.generateRandom((oWidth*2), (oHeight*2));
+    glOrtho(-orthoHalfWidth, orthoHalfWidth, -orthoHalfHeight, orthoHalfHeight, -1, 1);
+    m.generateRandom((orthoHalfWidth*2), (orthoHalfHeight*2));
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -108,7 +163,18 @@ void resizeScreen(int w, int h) {
 void keyDown(unsigned char key, int x, int y) {
     keyStates[key] = true;
 
-    if(key == 27) exit(0); //ESC
+    if(key == 'p' && !isExiting && !isRestarting) {
+        isPaused = !isPaused;
+    }
+    if(key == 'r') {
+        isRestarting = true;
+        isExiting = isPaused = false;
+    }
+
+    if(key == KEY_ESC) {
+        isExiting = true;
+        isRestarting = isPaused = false;
+    }
 }
 
 void specialKeyDown(int key, int x, int y) {
@@ -119,6 +185,14 @@ void keyUp(unsigned char key, int x, int y) {
     keyStates[key] = false;
     if(key == 'w') {
         s.setEngineOn(false);
+    }
+    if(key == 's') {
+        if(isExiting) exit(0);
+        if(isRestarting);//restart();
+    }
+    if(key == 'n') {
+        if(isExiting)    isExiting    = isPaused = false;
+        if(isRestarting) isRestarting = isPaused = false;
     }
 }
 
@@ -147,8 +221,12 @@ void keyboardHandle() {
 }
 
 void atualiza(int time) {
-    keyboardHandle();
     glutTimerFunc(time, atualiza, time);
+    if(isPaused || isRestarting || isExiting) {
+        glutPostRedisplay();
+        return;
+    }
+    keyboardHandle();
     if(m.collidesWith(s.getVertices())) {
         movement *= -0.0;
     }
@@ -174,7 +252,7 @@ int main(int argc, char **argv) {
     glutInitWindowPosition(0, 0);
 
     // Abre a janela
-    glutCreateWindow("Title");
+    glutCreateWindow("Lander");
     glutFullScreen();
 
     // Registra callbacks para alguns eventos
